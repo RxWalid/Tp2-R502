507311ae56bdac2ef10fa222b1b1687db5835af8
RxWalid <walidbellacene2002@gmail.com>
2023-10-02 12:40:14 +0200

    Cryptage Création de la première solution

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -1,15 +1,29 @@
-def affiche(n1, n2):
-    result = ""
-    for i in range(n1, n2 + 1):
-        output = ""
-        if i % 3 == 0:
-            output += "Fizz"
-        if i % 5 == 0:
-            output += "Buzz"
-        if not output:
-            output = str(i)
-        result += output
-    print(result)
+import unittest
 
-affiche(5, 10)
-affiche(10, 16)
+def crypt(message):
+    message_list = list(message)
+
+    for i in range(len(message_list)):
+        char = message_list[i]
+        if char.isalpha():
+            new_char = chr(ord(char) + 1)
+            message_list[i] = new_char
+
+    encrypted_message = ''.join(message_list)
+    return encrypted_message
+
+class TestCrypt(unittest.TestCase):
+    def test_crypt_lowercase(self):
+        self.assertEqual(crypt("abc"), "bcd")
+
+    def test_crypt_uppercase(self):
+        self.assertEqual(crypt("XYZ"), "YZA")
+
+    def test_crypt_mixed_case(self):
+        self.assertEqual(crypt("Hello World"), "Ifmmp Xpsme")
+
+    def test_crypt_non_alpha_characters(self):
+        self.assertEqual(crypt("123!@#"), "123!@#")
+
+if __name__ == '__main__':
+    unittest.main()
