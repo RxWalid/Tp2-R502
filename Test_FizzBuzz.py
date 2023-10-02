4de6e7cc48629c76663806fbeaf499aa52805da1
RxWalid <walidbellacene2002@gmail.com>
2023-10-02 12:30:22 +0200

    Evolution du code

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -1,5 +1,6 @@
-def affiche():
-    for i in range(1, 101):
+def affiche(n):
+    results = []
+    for i in range(1, n + 1):
         output = ""
         if i % 3 == 0:
             output += "Fizz"
@@ -7,6 +8,9 @@ def affiche():
             output += "Buzz"
         if not output:
             output = i
-        print(output)
-
-affiche()
+        results.append(output)
+    return results
+n = 100
+output_list = affiche(n)
+for item in output_list:
+    print(item)
