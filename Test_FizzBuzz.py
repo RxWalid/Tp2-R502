335dd9430572cf94a5413f4eca52f756a5abf9ad
RxWalid <walidbellacene2002@gmail.com>
2023-10-02 12:46:40 +0200

    Cryptage B. Première évolution du code

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -3,34 +3,38 @@ import unittest
 
 caracteres = string.ascii_letters + string.digits + string.punctuation + " "
 
-def crypt(message):
+def crypt(message, pas):
     message_crypte = ""
     for char in message:
         if char in caracteres:
             index = caracteres.index(char)
-            char_crypte = caracteres[(index + 1) % len(caracteres)]
+            char_crypte = caracteres[(index + pas) % len(caracteres)]
             message_crypte += char_crypte
         else:
             message_crypte += char
-    return message_crypte
+    return message_crypte + str(pas)
 
 def crypter_message_utilisateur():
     message = input("Entrez le message à crypter : ")
-    message_crypte = crypt(message)
-    print("Message crypté :", message_crypte)
+    pas = int(input("Entrez le décalage (entier entre 1 et 9) : "))
+    if 1 <= pas <= 9:
+        message_crypte = crypt(message, pas)
+        print("Message crypté :", message_crypte)
+    else:
+        print("Le décalage doit être un entier entre 1 et 9.")
 
 class TestCrypt(unittest.TestCase):
     def test_crypt_lowercase(self):
-        self.assertEqual(crypt("abc"), "bcd")
+        self.assertEqual(crypt("abc", 1), "bcd1")
 
     def test_crypt_uppercase(self):
-        self.assertEqual(crypt("XYZ"), "YZA")
+        self.assertEqual(crypt("XYZ", 3), "ABC3")
 
     def test_crypt_mixed_case(self):
-        self.assertEqual(crypt("Hello World"), "Ifmmp Xpsme")
+        self.assertEqual(crypt("Hello World", 5), "Mjqqt%&qtwhi5")
 
     def test_crypt_non_alpha_characters(self):
-        self.assertEqual(crypt("123!@#"), "123!@#")
+        self.assertEqual(crypt("123!@#", 2), "123!@#2")
 
 if __name__ == '__main__':
     choix = input("Voulez-vous crypter un message ? (oui/non) ")
