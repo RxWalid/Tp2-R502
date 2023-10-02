d00c93873b8b591d9204a6eb4163109076ae6685
RxWalid <walidbellacene2002@gmail.com>
2023-10-02 12:43:59 +0200

    Création de la première solution

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -1,16 +1,23 @@
+import string
 import unittest
 
+caracteres = string.ascii_letters + string.digits + string.punctuation + " "
+
 def crypt(message):
-    message_list = list(message)
+    message_crypte = ""
+    for char in message:
+        if char in caracteres:
+            index = caracteres.index(char)
+            char_crypte = caracteres[(index + 1) % len(caracteres)]
+            message_crypte += char_crypte
+        else:
+            message_crypte += char
+    return message_crypte
 
-    for i in range(len(message_list)):
-        char = message_list[i]
-        if char.isalpha():
-            new_char = chr(ord(char) + 1)
-            message_list[i] = new_char
-
-    encrypted_message = ''.join(message_list)
-    return encrypted_message
+def crypter_message_utilisateur():
+    message = input("Entrez le message à crypter : ")
+    message_crypte = crypt(message)
+    print("Message crypté :", message_crypte)
 
 class TestCrypt(unittest.TestCase):
     def test_crypt_lowercase(self):
@@ -26,4 +33,8 @@ class TestCrypt(unittest.TestCase):
         self.assertEqual(crypt("123!@#"), "123!@#")
 
 if __name__ == '__main__':
-    unittest.main()
+    choix = input("Voulez-vous crypter un message ? (oui/non) ")
+    if choix.lower() == "oui":
+        crypter_message_utilisateur()
+    else:
+        unittest.main()
